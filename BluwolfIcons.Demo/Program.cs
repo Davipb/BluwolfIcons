2cfa62f5d4f1c255c84c08c259a70aaa4114ff53
Davipb <daviparca@gmail.com>
2016-05-31 19:58:11 -0300

    Added Demo

---
+++ BluwolfIcons.Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace BluwolfIcons.Demo
+{
+	class Program
+	{
+		static void Main()
+		{
+			var images = Directory.EnumerateFiles("./", "*.png");
+
+			var icon = new Icon();
+			foreach (var image in images)
+			{
+				var bitmap = new System.Drawing.Bitmap(image);
+
+				icon.Images.Add(new PngIconImage(bitmap));
+				icon.Images.Add(new BmpIconImage(bitmap));
+			}
+
+			icon.Save("icon.ico");
+			Console.WriteLine($"Wrote {icon.Images.Count} icon images to 'icon.ico'.");
+			Console.ReadKey();
+		}
+	}
+}
