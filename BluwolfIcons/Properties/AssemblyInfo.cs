dd5496d99b4ba608636403886ed2ade7a9a4bb2e
Davipb <daviparca@gmail.com>
2016-05-31 18:30:35 -0300

    Update Assembly Info

--- BluwolfIcons/Properties/AssemblyInfo.cs
+++ BluwolfIcons/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BluwolfIcons")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET library for manipulating icon files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BluwolfIcons")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Davipb")]
+[assembly: AssemblyProduct("Bluwolf Icons")]
+[assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
