fc179b8aa57ab9fae2e040d463a0e184f72d16d2
Davipb <daviparca@gmail.com>
2016-06-06 21:10:45 -0300

    Minor cleanup

--- BluwolfIcons/Icon.cs
+++ BluwolfIcons/Icon.cs
@@ -11,8 +11,6 @@ namespace BluwolfIcons
 	/// </summary>
 	public sealed class Icon
 	{
-		private static readonly byte[] PngFileHeader = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
-
 		/// <summary>
 		/// All the images contained in this icon.
 		/// </summary>
